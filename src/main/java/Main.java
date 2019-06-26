1b177230a0c2ad91ce085f5da2f37e02b4981610
golfarn92 <mattias.hedin@gmail.com>
2019-06-26 10:03:27 +0200

    Added branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("matte test");
         try {
             DefaultTerminalFactory dtf = new DefaultTerminalFactory();
             Terminal terminal = dtf.createTerminal();
