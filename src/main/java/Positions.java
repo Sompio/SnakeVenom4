b0b2c2e668cf399eaeaa81bf2d8a631f921a58fa
ajvin <irvin_chu@live.se>
2019-06-28 14:56:07 +0200

    Positions class

--- src/main/java/Positions.java
+++ src/main/java/Positions.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Positions {
+
+    class Position {
+        public int x;
+        public int y;
+
+        public Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
 }
